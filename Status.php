2c90310bdd878c6d3645423a25653daa1fa74e58
Yunnen <miglioriniguilhermegallo@gmail.com>
2023-04-15 18:07:11 -0300

    Create Status.php

---
+++ Status.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="CSS\Status.css">
+    <title>Document</title>
+</head>
+<body>
+    <header class="Status">
+        <div class="ficha">
+        <table class="Status" border="0">
+        <tr>
+            <td class="label-1">Nome:
+            <label for="Nome"> Alison Cooper</label>
+            </td>
+            
+        </tr>
+        <tr>
+        
+        <td class="label-1"> Nível:
+        <label for="Nivel">5</label>
+        </td>
+
+    </tr>
+
+        <td class="label-1"> Classe: 
+        <label for="Classe">Warden</label></td>
+    </tr>
+        
+        <td class="label-1">Tendencia: 
+        <label for="Tendencia">Neutro Leal</label></td>
+    </tr>
+        </table>
+        <div class="ficha2">
+    <tr>
+        <td class="label-1">Aliados:
+        <ul>
+                <li class="list" id="Status"> 
+                    <a href="personagens.php" target="Persona">
+                    <i class="fas fa-address-card" ></i>
+                    <span class="text">Alin</span>
+                    </a>
+                    
+                    <li class="list" id="Status"> 
+                    <a href="#" target="Persona">
+                    <i class="fas fa-address-card" ></i>
+                    <span class="text">Damian</span>
+                    </a>
+                    
+                    <li class="list" id="Status"> 
+                    <a href="#" target="testframe">
+                    <i class="fas fa-address-card" ></i>
+                    <span class="text">Emlily</span>
+                    </a>
+            
+    </tr>
+        
+    </div>
+</div>
+<script src="https://kit.fontawesome.com/1c8b0a3809.js" crossorigin="anonymous"></script>
+<iframe class="personagens" src="calculoIMC.php" name="Persona"></iframe>
+  <!--  
+    <div class="atributos">
+        <label class="titulo">Atributos Geneticos</label>
+    <div class="atri">
+    
+    <fieldset>
+    <legend>FOR</legend>
+        <label class="c">15</label><br>
+        <label class="C">3</label>
+    </filedset><br>
+    
+    <fieldset>
+        <legend>DEX</legend>
+        <label class="c">15</label><br>
+        <label class="C">3</label>
+    </filedset><br>
+    
+    <fieldset>
+    <legend>RES</legend>
+        <label class="c">15</label><br>
+        <label class="C">3</label>
+    </filedset><br>
+    
+    <fieldset>
+    <legend>CONS</legend>
+        <label class="c">15</label><br>
+        <label class="C">3</label>
+    </filedset><br>
+    </div>
+    
+    <div class="atri2">
+    
+    <fieldset>
+    <legend>CAR</legend>
+        <label class="c">15</label><br>
+        <label class="C">3</label>
+    </filedset><br>
+    
+    <fieldset>
+    <legend>SAB</legend>
+
+        <label class="c">15</label><br>
+        <label class="C">3</label>
+    </filedset><br>
+    
+    <fieldset>
+    <legend>INT</legend>
+    
+        <label class="c">15</label><br>
+        <label class="C">3</label>
+    </filedset><br>
+    
+    <fieldset>
+    <legend>PDM</legend>
+        <label class="c">15</label><br>
+        <label class="C">3</label>
+    </filedset><br>
+    
+</div>
+-->
+  
+    
+</div>
+</header>
+    
+</body>
+</html>
